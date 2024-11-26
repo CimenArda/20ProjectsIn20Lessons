7c5e1b085194207fcc1e86f7187cfccb33cd9765
ArdaCimen <cimenarda.yaz@gmail.com>
2024-11-26 13:44:04 +0300

    Rapid Api Hava Durumu Kartı Projesi

---
+++ WeatherCardProject/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace WeatherCardProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblwindSpeed = new System.Windows.Forms.Label();
+            this.lblhumidity = new System.Windows.Forms.Label();
+            this.lblfahrenheit = new System.Windows.Forms.Label();
+            this.lblcelcius = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WeatherCardProject.Properties.Resources.sun;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 206);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(117, 55);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(31, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ISTANBUL,TR";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(29, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 32);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Bugün";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(31, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ISTANBUL,TR";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 299);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Rüzgar:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 340);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 23);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Nem:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 377);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 23);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Fahrenheit:";
+            // 
+            // lblwindSpeed
+            // 
+            this.lblwindSpeed.AutoSize = true;
+            this.lblwindSpeed.Location = new System.Drawing.Point(128, 297);
+            this.lblwindSpeed.Name = "lblwindSpeed";
+            this.lblwindSpeed.Size = new System.Drawing.Size(33, 23);
+            this.lblwindSpeed.TabIndex = 9;
+            this.lblwindSpeed.Text = "00";
+            // 
+            // lblhumidity
+            // 
+            this.lblhumidity.AutoSize = true;
+            this.lblhumidity.Location = new System.Drawing.Point(128, 340);
+            this.lblhumidity.Name = "lblhumidity";
+            this.lblhumidity.Size = new System.Drawing.Size(33, 23);
+            this.lblhumidity.TabIndex = 10;
+            this.lblhumidity.Text = "00";
+            // 
+            // lblfahrenheit
+            // 
+            this.lblfahrenheit.AutoSize = true;
+            this.lblfahrenheit.Location = new System.Drawing.Point(128, 377);
+            this.lblfahrenheit.Name = "lblfahrenheit";
+            this.lblfahrenheit.Size = new System.Drawing.Size(33, 23);
+            this.lblfahrenheit.TabIndex = 11;
+            this.lblfahrenheit.Text = "00";
+            // 
+            // lblcelcius
+            // 
+            this.lblcelcius.AutoSize = true;
+            this.lblcelcius.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblcelcius.Location = new System.Drawing.Point(279, 203);
+            this.lblcelcius.Name = "lblcelcius";
+            this.lblcelcius.Size = new System.Drawing.Size(124, 46);
+            this.lblcelcius.TabIndex = 12;
+            this.lblcelcius.Text = "00.00";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(395, 180);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(20, 18);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "C";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(354, 259);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(69, 23);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "Bugün";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(488, 468);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.lblcelcius);
+            this.Controls.Add(this.lblfahrenheit);
+            this.Controls.Add(this.lblhumidity);
+            this.Controls.Add(this.lblwindSpeed);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblwindSpeed;
+        private System.Windows.Forms.Label lblhumidity;
+        private System.Windows.Forms.Label lblfahrenheit;
+        private System.Windows.Forms.Label lblcelcius;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ WeatherCardProject/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http.Headers;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherCardProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://open-weather13.p.rapidapi.com/city/Istanbul/TR"),
+                Headers =
+    {
+        { "x-rapidapi-key", "ff6bd20e1emshe43dd1aa8d4e77cp1185e1jsnb9d0d86109a6" },
+        { "x-rapidapi-host", "open-weather13.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var json = JObject.Parse(body);
+                var fahrenheit = json["main"]["feels_like"].ToString();
+                var windSpeed = json["wind"]["speed"].ToString();
+                var humadity = json["main"]["humidity"].ToString();
+                lblfahrenheit.Text = fahrenheit;
+                lblhumidity.Text = humadity;
+                lblwindSpeed.Text = windSpeed;
+
+                double celcius = (double.Parse(fahrenheit) - 32);
+
+                double celciusvalue = celcius / 1.8;
+                lblcelcius.Text = celciusvalue.ToString();
+            }
+        }
+    }
+}
